94a42f28e28a164b8b01fbcb6563a40d54cc0ae2
SFSM565826960 <sfsm565826960@gmail.com>
2017-04-16 21:58:36 +0800

    add utils http.js

---
+++ server/utils/http.js
@@ -0,0 +1,69 @@
+/**
+ * 此模块用于Http的Get和Post请求
+ */
+
+var request = require('request');
+var iconv = require('iconv-lite');
+
+/**
+ * 获取Charset，仅GBK，GB2313，UTF8有效，
+ * 若为空或其他编码，默认返回UTF8
+ */
+var getCharset = function (response) {
+  var contentType = (response.headers || {})['content-type'];
+  if (!contentType || contentType.length === 0) {
+    return 'UTF8';
+  } else {
+    var charset = contentType.replace(/ /g, '').match(/charset=(\w+)/i);
+    if (charset && charset.length > 1) {
+      return charset[1];
+    } else {
+      return 'UTF8';
+    }
+  }
+}
+
+/**
+ * GET请求
+ * @param {String} url 请求的URL地址
+ * @param {Function} callback 请求回调函数，function(err, body, response)
+ */
+var get = function(url, callback) {
+  request.get({
+    url: url,
+    encoding: null
+  }, function(error, response, body) {
+    if (error) {
+      console.error(error);
+      callback(error, body, response);
+    } else {
+      callback(null, iconv.decode(body, getCharset(response)));
+    }
+  })
+}
+
+/**
+ * POST请求
+ * @param {String} url 请求的URL地址
+ * @param {Object} data 发送的数据（JSON）
+ * @param {Function} callback 请求回调函数，function(err, body, response)
+ */
+var post = function(url, data, callback) {
+  request.post({
+    url: url,
+    form: data,
+    encoding: null
+  }, function(error, response, body) {
+    if (error) {
+      console.error(error);
+      callback(error, body, response);
+    } else {
+      callback(null, iconv.decode(body, getCharset(response)));
+    }
+  })
+}
+
+module.exports = {
+  get: get,
+  post: post
+};
