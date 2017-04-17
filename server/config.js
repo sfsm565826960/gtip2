393ccb253d3c5fb2eb93b6d6ba08326193b6293e
SFSM565826960 <sfsm565826960@gmail.com>
2017-04-17 18:00:55 +0800

    fix config dbPath

--- server/config.js
+++ server/config.js
@@ -1,11 +1,24 @@
 var path = require('path');
 
+/**
+ * 修复在gtip目录下和gtip/server目录下获取dbPath绝对路径不一致的问题
+ */
+var basePath = path.resolve('.');
+var result = basePath.match(/.+?server/);
+if (result) {
+  basePath = result[0];
+} else {
+  basePath = path.resolve('server');
+}
+
 exports.DB = {
   host: '127.0.0.1',
   port: 27077,
-  dbPath: path.resolve('localdb/data/'),
-  logPath: path.resolve('localdb/log/db.log'),
+  dbPath: path.resolve(basePath, './localdb/data/'),
+  logPath: path.resolve(basePath, './localdb/log/db.log'),
   db: 'gtip',
   user: 'gtip',
   password: '8a3a2a7e'
 }
+
+console.log(JSON.stringify(exports.DB));
