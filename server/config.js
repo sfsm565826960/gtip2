74abd9381a09fc590508dc20eae0fd7490600bab
SFSM565826960 <sfsm565826960@gmail.com>
2017-04-17 12:42:28 +0800

    add config.js add tickTest

---
+++ server/config.js
@@ -0,0 +1,7 @@
+exports.DB = {
+  host: '127.0.0.1',
+  port: 27077,
+  db: 'gtip',
+  user: 'gtip',
+  password: '8a3a2a7e'
+}
---
+++ server/test/tick.js
@@ -0,0 +1,82 @@
+/**
+ * 测试timeout和interval的稳定性
+ */
+
+var MongoClient = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
+var CONF_DB = require('../config.js').DB;
+
+var client = new MongoClient(new Server(CONF_DB.host, CONF_DB.port, {
+  socketOptions: {
+    connectTimeoutMS: 500
+  },
+  poolSize: 5,
+  auto_reconnect: true
+}), {
+  numberOfRetries: 3,
+  retryMillSeconds: 500
+});
+
+var collectionTickTest = null;
+var setIntervalIndex = 0;
+var setTimeoutIndex = 0;
+
+function runTest() {
+  if (!collectionTickTest) return;
+  console.info('tickTest start');
+  setInterval(function(){
+    collectionTickTest.insert({
+      type: 'setInterval',
+      time: new Date().getTime(),
+      index: ++setIntervalIndex
+    }, function(err, results) {
+      if (err) {
+        console.error('insert fail: ' + err, err);
+      }
+    });
+  }, 1000);
+
+  var timeoutInsert = function() {
+    collectionTickTest.insert({
+      type: 'setTimeout',
+      time: new Date().getTime(),
+      index: ++setTimeoutIndex
+    }, function(err, results) {
+      if (err) {
+        console.error('insert fail: ' + err, err);
+      }
+    })
+    setTimeout(timeoutInsert, 1000);
+  }
+  setTimeout(timeoutInsert, 1000);
+}
+
+client.open(function(err, con) {
+  if (err) {
+    console.error('Connect fail: ' + err, err)
+  } else {
+    var db = con.db(CONF_DB.db);
+    if (db) {
+      db.authenticate(CONF_DB.user, CONF_DB.password, function(err, result) {
+        if (err) {
+          console.error('DB user authenticate fail: ' + err, err);
+          client.close();
+          console.log('Connect closed');
+        } else {
+          db.collection('tickTest', {}, function(err, collection) {
+            if (err) {
+              console.error('Access collection tickTest fail: ' + err, err);
+              client.close();
+              console.log('Connect closed');
+            } else {
+              collectionTickTest = collection;
+              runTest();
+            }
+          })
+        }
+      })
+    } else {
+      console.error('Cannot access db gtip')
+    }
+  }
+});
