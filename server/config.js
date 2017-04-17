6ded54d15d687bc61dccb31f34991cf28c96caaa
SFSM565826960 <sfsm565826960@gmail.com>
2017-04-17 17:19:57 +0800

    fix db

--- server/config.js
+++ server/config.js
@@ -1,6 +1,10 @@
+var path = require('path');
+
 exports.DB = {
   host: '127.0.0.1',
   port: 27077,
+  dbPath: path.resolve('localdb/data/'),
+  logPath: path.resolve('localdb/log/db.log'),
   db: 'gtip',
   user: 'gtip',
   password: '8a3a2a7e'
