bb968008dbc38ac69881bf1a3612919f354cd5a6
Franchen <isworkinghard@163.com>
2017-04-25 22:49:46 +0800

    add utils push config

--- server/config.js
+++ server/config.js
@@ -5,13 +5,13 @@ var path = require('path');
  */
 var basePath = path.resolve('.');
 var result = basePath.match(/.+?server/);
-if (result) {
-  basePath = result[0];
-} else {
-  basePath = path.resolve('server');
-}
-
+if (result) { basePath = result[0]; } else { basePath = path.resolve('server'); }
 exports.SERVER_PATH = path.resolve(basePath);
+
+exports.APP = {
+  name: 'gtip',
+  logo: 'gtip.png'
+}
 
 exports.DB = {
   host: '127.0.0.1',
@@ -24,9 +24,16 @@ exports.DB = {
 }
 
 exports.MAIL = {
-  enable: true,
+  enable: false,
   host: 'smtp.sina.com',
   user: 'sfsm_gtip@sina.com',
   pwd: 'sfsm_gtip',
   admin: '1078777529@qq.com'
+}
+
+exports.PUSH = {
+  host: 'http://sdk.open.api.igexin.com/apiex.htm',
+  appId: '8q1qy5UUU86JheqOeJBrb', // '6wwCkjEpyx7MO292BIjzH2',
+  appKey: 'a2tjYW6pLQ8QZh8I2PXwZ3', // 'aQYm3ENv8WAHHuN5nVqtd5',
+  masterSecret: 'XeYdAikOXvAJyYAcWPJbB7' // 'ilP1eCl1RD8BLcfsC7JBfA'
 }
