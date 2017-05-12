e0100639f1858a893d0e5ea135a11bc508a4c339
SFSM565826960 <sfsm565826960@gmail.com>
2017-05-12 15:23:38 +0800

    fix api

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var modelLoaded = false;
+var Log = require('../utils/log')({
+  file: 'api.log'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
