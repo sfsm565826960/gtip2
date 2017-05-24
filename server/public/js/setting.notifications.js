1786ac81745d40391e611fd2d6002287f45ef536
SFSM565826960 <sfsm565826960@gmail.com>
2017-05-24 18:07:28 +0800

    fix

--- server/public/js/setting.notifications.js
+++ server/public/js/setting.notifications.js
@@ -6,12 +6,11 @@ mui.plusReady(function() {
 	var isSettingsChange = false; // 是否更改了设置
 	//新信息通知
 	for(key in notifications) {
-		var elm = doc.getElementById(key);
+		var elm = document.getElementById(key);
 		if(!elm) {
 			console.error(key + 'has no match element');
 			continue;
 		}
-		console.log('add:' + key);
 		elm.addEventListener("toggle", function(e) {
 			modifyData[e.target.id] = e.detail.isActive;
 			isSettingsChange = true;
@@ -22,29 +21,27 @@ mui.plusReady(function() {
 			var settings = App.getSettings();
 			var notifications = settings.notifications || {};
 			for(key in notifications) {
-				var elm = doc.getElementById(key);
+				var elm = document.getElementById(key);
 				if(elm) elm.classList[notifications[key] === true ? 'add' : 'remove']('mui-active');
 			}
 		}
 	});
 	viewApi.view.addEventListener('pageBack', function(e) {
-		console.log(isSettingsChange)
 		if(e.detail.page.id === 'notifications') {
 			if(isSettingsChange) {
-				console.log(JSON.stringify(modifyData));
 				Server.send('settings/notifications', {
 					token: App.getToken(),
 					settings: JSON.stringify(modifyData)
 				}, function(res) {
 					if(res) {
 						if(res.state == 'ok') {
-							$.toast('设置提醒成功');
+							mui.toast('同步设置成功');
 							App.setSettings(res.data);
 						} else {
-							$.toast(res.detail);
+							mui.toast(res.detail);
 						}
 					} else {
-						$.toast('服务器返回格式错误');
+						mui.toast('服务器返回格式错误');
 					}
 				});
 				modifyData = {};
